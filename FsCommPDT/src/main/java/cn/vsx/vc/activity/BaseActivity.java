d6e3381cf9d2b1e1e1a49b8b57608705c89dc4f9
yueshaolong <yueshaolong@vsxin.com>
2019-01-22 09:38:54 +0800

    适配9.0的弹窗关闭

--- FsCommPDT/src/main/java/cn/vsx/vc/activity/BaseActivity.java
+++ FsCommPDT/src/main/java/cn/vsx/vc/activity/BaseActivity.java
@@ -31,6 +31,8 @@ import com.zectec.imageandfileselector.utils.OperateReceiveHandlerUtilSync;
 
 import org.apache.log4j.Logger;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.List;
@@ -228,7 +230,8 @@ public abstract class BaseActivity extends AppCompatActivity implements RecvCall
 					| PowerManager.SCREEN_DIM_WAKE_LOCK, "wakeLock");
 
 		}
-
+		//适配Android9.0调用hide时，关闭警告弹窗
+		closeAndroidPDialog();
 	}
 
 	protected void protectApp(){
@@ -571,5 +574,24 @@ public abstract class BaseActivity extends AppCompatActivity implements RecvCall
 			}
 		}
 	};
-
+	private void closeAndroidPDialog(){
+		try {
+			Class aClass = Class.forName("android.content.pm.PackageParser$Package");
+			Constructor declaredConstructor = aClass.getDeclaredConstructor(String.class);
+			declaredConstructor.setAccessible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		try {
+			Class cls = Class.forName("android.app.ActivityThread");
+			Method declaredMethod = cls.getDeclaredMethod("currentActivityThread");
+			declaredMethod.setAccessible(true);
+			Object activityThread = declaredMethod.invoke(null);
+			Field mHiddenApiWarningShown = cls.getDeclaredField("mHiddenApiWarningShown");
+			mHiddenApiWarningShown.setAccessible(true);
+			mHiddenApiWarningShown.setBoolean(activityThread, true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
