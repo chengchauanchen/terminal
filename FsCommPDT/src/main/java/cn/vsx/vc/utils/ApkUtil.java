482949d919fb295e36e70d2fb598fe3be73fc3bd
xiaolong125 <2247081635@qq.com>
2019-06-18 15:08:00 +0800

    提交工具类

---
+++ FsCommPDT/src/main/java/cn/vsx/vc/utils/ApkUtil.java
@@ -0,0 +1,21 @@
+package cn.vsx.vc.utils;
+
+import cn.vsx.hamster.terminalsdk.TerminalFactory;
+import cn.vsx.hamster.terminalsdk.manager.auth.AuthManagerTwo;
+import cn.vsx.hamster.terminalsdk.tools.Params;
+
+/**
+ * 作者：ly-xuxiaolong
+ * 版本：1.0
+ * 创建日期：2019/6/18
+ * 描述：
+ * 修订历史：
+ */
+public class ApkUtil{
+
+    //是否为市局的包
+    public static boolean isPoliceApk(){
+        String apkType = TerminalFactory.getSDK().getParam(Params.APK_TYPE, AuthManagerTwo.POLICESTORE);
+        return apkType.equals(AuthManagerTwo.POLICESTORE) || apkType.equals(AuthManagerTwo.POLICETEST);
+    }
+}
