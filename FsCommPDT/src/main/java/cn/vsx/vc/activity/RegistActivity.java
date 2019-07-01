16c57f5e90eda7f0aa5b21563daa7caca678104c
yangpei <yangpei@vsxin.com>
2019-07-01 09:22:57 +0800

    1.重新设置设备类型 2.修改版本号

--- FsCommPDT/src/main/java/cn/vsx/vc/activity/RegistActivity.java
+++ FsCommPDT/src/main/java/cn/vsx/vc/activity/RegistActivity.java
@@ -1143,6 +1143,7 @@ public class RegistActivity extends BaseActivity implements RecvCallBack, Action
 
         } else {
             TerminalFactory.getSDK().putParam(Params.POLICE_STORE_APK, false);
+            MyApplication.instance.setTerminalMemberType();
             String apkType = TerminalFactory.getSDK().getParam(Params.APK_TYPE,AuthManagerTwo.POLICESTORE);
             if(AuthManagerTwo.POLICESTORE.equals(apkType) || AuthManagerTwo.POLICETEST.equals(apkType)|| AuthManagerTwo.XIANGYANGPOLICESTORE.equals(apkType)){
                 ToastUtil.showToast(MyApplication.instance.getApplicationContext(), getString(R.string.text_please_open_wuhan_police_work_first));
