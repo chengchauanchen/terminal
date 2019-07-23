81b7cf7443944266bece32579c6b6c9beeae2d3e
xiaolong125 <2247081635@qq.com>
2019-07-23 15:50:31 +0800

    增加无人机云台和指南针校准

--- uav/src/main/java/cn/vsx/uav/utils/AirCraftUtil.java
+++ uav/src/main/java/cn/vsx/uav/utils/AirCraftUtil.java
@@ -2,10 +2,17 @@ package cn.vsx.uav.utils;
 
 import android.text.TextUtils;
 
+import com.blankj.utilcode.util.ToastUtils;
+
 import org.apache.log4j.Logger;
 
+import java.util.List;
+
+import dji.common.flightcontroller.CompassCalibrationState;
 import dji.common.flightcontroller.LocationCoordinate3D;
 import dji.sdk.base.BaseProduct;
+import dji.sdk.flightcontroller.Compass;
+import dji.sdk.gimbal.Gimbal;
 import dji.sdk.products.Aircraft;
 import dji.sdk.sdkmanager.DJISDKManager;
 
@@ -20,7 +27,7 @@ public class AirCraftUtil{
 
     private static Logger logger = Logger.getLogger(AirCraftUtil.class);
 
-    public static boolean isAircraftConnected() {
+    private static boolean isAircraftConnected() {
         return getProductInstance() != null && getProductInstance() instanceof Aircraft;
     }
 
@@ -51,6 +58,8 @@ public class AirCraftUtil{
             if(checkLatitude(location.getLatitude()) && checkLongitude(location.getLongitude())){
                 sb.append(location.getLatitude()).append(",").append(location.getLongitude()).append(",").append(location.getAltitude());
             }
+
+
         }
         logger.info("无人机位置："+sb.toString());
         return sb.toString();
@@ -89,5 +98,99 @@ public class AirCraftUtil{
 
     public static boolean checkLongitude(double longitude){
         return longitude !=0.0 && !Double.isNaN(longitude) && longitude >=- 180 && longitude <= 180;
+    }
+
+    /**
+     * @return 指南针是否需要校准
+     */
+    @SuppressWarnings("unused")
+    public static synchronized boolean checkCompass(){
+        boolean hasError = false;
+        Aircraft aircraft = getAircraftInstance();
+        if(aircraft !=null){
+            Compass compass = aircraft.getFlightController().getCompass();
+            hasError = compass.hasError();
+        }
+        return hasError;
+    }
+
+    /**
+     * 校准指南针
+     */
+    @SuppressWarnings("unused")
+    public static synchronized void calibratCompass(){
+        Aircraft aircraft = getAircraftInstance();
+        if(aircraft !=null){
+            Compass compass = aircraft.getFlightController().getCompass();
+            boolean hasError = compass.hasError();
+            if(hasError){
+                //校准回调
+                compass.setCalibrationStateCallback(compassCalibrationState -> {
+                    if(compassCalibrationState == CompassCalibrationState.HORIZONTAL){
+                        //指南针水平校准。用户应水平握住飞机并将其旋转360度。
+                        ToastUtils.showShort("水平握住飞机并将其旋转360度");
+                    }else if(compassCalibrationState == CompassCalibrationState.VERTICAL){
+                        //指南针垂直校准。使用者应垂直握住飞机，使机头指向地面，并将飞机旋转360度。
+                        ToastUtils.showShort("垂直握住飞机，使机头指向地面，并将飞机旋转360度");
+                    }else if(compassCalibrationState == CompassCalibrationState.SUCCESSFUL){
+                        //指南针校准成功
+                        ToastUtils.showShort("指南针校准成功");
+                    }else if(compassCalibrationState == CompassCalibrationState.FAILED){
+                        //指南针校准失败。确保指南针附近没有磁铁或金属物体，然后重试。
+                        ToastUtils.showShort("指南针校准失败。确保指南针附近没有磁铁或金属物体，然后重试");
+                    }else if(compassCalibrationState == CompassCalibrationState.NOT_CALIBRATING){
+                        //正常状态。指南针不在校准中。
+                        ToastUtils.showShort("正常状态。指南针不在校准中");
+                    }else if(compassCalibrationState == CompassCalibrationState.UNKNOWN){
+                        //指南针校准状态未知。
+                        ToastUtils.showShort("指南针校准状态未知");
+                    }
+                });
+                compass.startCalibration(djiError -> {
+                    if(djiError == null){
+                        //开始校准
+                        ToastUtils.showShort("开始校准");
+                    }else {
+                        logger.error("校准指南针出错："+djiError);
+                    }
+                });
+            }
+        }
+    }
+
+    /**
+     * 校准云台
+     */
+    @SuppressWarnings("unused")
+    public static synchronized void calibratGimbals(){
+        Aircraft aircraft = getAircraftInstance();
+        if(aircraft !=null){
+            List<Gimbal> gimbals = aircraft.getGimbals();
+            logger.info("云台个数："+gimbals.size());
+            for(Gimbal gimbal : gimbals){
+                calibratGimbal(gimbal);
+            }
+        }
+    }
+
+    private static synchronized void calibratGimbal(Gimbal gimbal){
+        gimbal.setStateCallback(gimbalState -> {
+            int calibrationProgress = gimbalState.getCalibrationProgress();
+            logger.info("calibrationProgress:"+calibrationProgress);
+            boolean calibrationSuccessful = gimbalState.isCalibrationSuccessful();
+            if(calibrationSuccessful){
+                ToastUtils.showShort("云台校准成功");
+            }else {
+                ToastUtils.showShort("云台校准失败");
+            }
+        });
+        gimbal.startCalibration(djiError -> {
+            if(djiError == null){
+                ToastUtils.showShort("云台开始校准");
+            }else {
+                ToastUtils.showShort("云台校准失败");
+                logger.error("云台校准失败:"+djiError);
+            }
+        });
     }
 }
